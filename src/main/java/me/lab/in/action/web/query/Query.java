b474767754cf0b62365b8decb55fb5de6487c0d8
sadolin <sadolintw@gmail.com>
2020-10-12 23:49:07 +0800

    add json input

--- src/main/java/me/lab/in/action/web/query/Query.java
+++ src/main/java/me/lab/in/action/web/query/Query.java
@@ -1,16 +1,21 @@
 package me.lab.in.action.web.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Query implements GraphQLQueryResolver {
 
     public String firstQuery(){
-        return "First Query";
+        return "First Query.";
     }
 
     public String secondQuery(){
         return "Second Query";
+    }
+
+    public String fullNameQuery(@NotNull SampleRequest sampleRequest){
+        return sampleRequest.getFirstName() + " " + sampleRequest.getLastName();
     }
 }
